1f10b97eb4b2e0d90cbdbd990b6458d2df26f572
Pierre Segalen <psegalen@outlook.com>
2025-04-24 12:23:50 +0200

    Added cache first strategy on distant images

--- front/cacheSW.js
+++ front/cacheSW.js
@@ -8,12 +8,25 @@ self.addEventListener("install", (event) => {
 });
 
 const checkNetworkRequest = async (request) => {
-  if (request.url.startsWith("http")) {
-    console.log(request);
-  }
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
+  if (
+    request.url.startsWith("http") &&
+    request.destination === "image"
+  ) {
+    console.log("An image has been requested", request);
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      console.log("An image was served from the cache!");
+      return responseFromCache;
+    }
+    try {
+      const networkResponse = await fetch(request);
+      const cache = await caches.open("v1");
+      cache.put(request, networkResponse.clone());
+      console.log("An image was added to the cache!");
+      return networkResponse;
+    } catch (error) {
+      return Response.error();
+    }
   }
   return fetch(request);
 };
